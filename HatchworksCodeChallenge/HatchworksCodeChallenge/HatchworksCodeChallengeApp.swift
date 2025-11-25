74d48e4f06b8dc5ea345c911f30ca643a9e4d3d1
Juan David <juandavidl2011.jdll@gmail.com>
2025-11-24 21:44:13 -0500

    Initial Commit
    Starting project

---
+++ HatchworksCodeChallenge/HatchworksCodeChallenge/HatchworksCodeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HatchworksCodeChallengeApp.swift
+//  HatchworksCodeChallenge
+//
+//  Created by Juan David Lopera Lopez on 24/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HatchworksCodeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
